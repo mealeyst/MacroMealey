0c18255353816562f94915100b7055c3fbb52821
Shawn Mealey <mealeyst@gmail.com>
2016-10-21 23:23:00 -0400

    rebuilding site Fri Oct 21 23:23:00 EDT 2016

--- deploy.sh
+++ deploy.sh
@@ -18,4 +18,4 @@ git commit -m "$msg"
 # Push source and build repos.
 git push origin master
 # git subtree push --prefix=public git@github.com:mealeyst/macromealey.git gh-pages
-git push origin `git subtree split --prefix public git@github.com:mealeyst/macromealey.git`:gh-pages --force
+git push origin `git subtree split --prefix public gh-pages`:gh-pages --force
