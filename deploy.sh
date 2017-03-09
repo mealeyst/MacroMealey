96a4139b5f1e4e0bea743d8e9e6a59675d5b6aad
Shawn Mealey <mealeyst@gmail.com>
2017-03-09 14:31:15 -0500

    Checking to see if deployments delete gh-pages branch

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,5 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-git push origin :gh-pages
-git subtree push --prefix=public git@github.com:mealeyst/macromealey.git gh-pages
+git subtree push --prefix=public git@github.com:mealeyst/macromealey.git gh-pages -f
 # git push origin `git subtree split --prefix public gh-pages`:gh-pages --force
